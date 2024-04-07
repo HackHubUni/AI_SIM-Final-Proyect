b4e2c6865386d9934626f7316bf6ab53ac3b6a3e
carlapvalera <perezvaleracarlas@gmail.com>
2024-04-06 20:18:31 -0400

    creacion de los productos

--- .idea/src/protocol/producerprotocol.py
+++ .idea/src/protocol/producerprotocol.py
@@ -1,13 +1,53 @@
 from collections import abc
 
-class Producer(abc.Protocol):
+class Product(abc.Protocol):
 
-    def produce(self):
-         # uqe produce str
-
+    def price(self):
          pass
     
-    def acount(self, amount):
-        #dado la cant que necesito cual seria el precio
+    def proper_temperature(self, temperature):
+        #si la temperatura wes adecuada
         pass    
+    def proper_humidity(self, humidity):
+        pass
 
+class BaseProduct(Product):
+    def __init__(self, name, quality, gross_price, ideal_temperature,ideal_humidity, time_to_produce):
+        self.name = name
+        self.quality = quality
+        self.gross_price = gross_price
+        self.ideal_temperature = ideal_temperature
+        self.time_to_produce = time_to_produce
+        
+        self.ideal_humidity = ideal_humidity
+
+    
+    def price(self):
+        return self.quality * self.gross_price
+    
+    def proper_temperature(self, temperature):
+        if not self.ideal_temperature - temperature < 2:
+            self.quality -= 1
+    
+    def proper_humidity(self, humidity):
+        if not self.ideal_humidity - humidity < 2:
+            self.quality -= 1
+    
+class ProcessedProduct(BaseProduct, Product):
+    def __init__(self, name, quality, gross_price, time_to_produce, ideal_temperature, ideal_humidity,ingredients):
+        super().__init__(name, quality, gross_price, ideal_temperature, ideal_humidity, time_to_produce)
+        self.ingredients = ingredients
+
+    def price(self):
+        return self.quality * self.gross_price
+    
+    def proper_temperature(self, temperature):
+        if not self.ideal_temperature - temperature < 2:
+            self.quality -= 1
+
+    def proper_humidity(self, humidity):
+        if not self.ideal_humidity - humidity < 2:
+            self.quality -= 1
+
+
+    
