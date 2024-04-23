95b24a856a314f299bcd252a95b7a48c7a943ee5
carlapvalera <perezvaleracarlas@gmail.com>
2024-04-23 16:29:29 -0400

    plans

--- supply_chain/agents/func.py
+++ supply_chain/agents/func.py
@@ -9,12 +9,13 @@ class Func:
     def __str__(self):
         return f"Event type: {self.event_type}, Time: {self.event_time}, Location: {self.event_location}, Description: {self.event_description}"
     
-class Func_restock:
-    def __init__(self, event_type, event_time, event_location, event_description,price):
+class Func_restock(Func):
+    def __init__(self, event_type, event_time, event_location, event_description,ingredient,price):
         self.event_type = event_type
         self.event_time = event_time
         self.event_location = event_location
         self.event_description = event_description
+        self.ingredients = ingredient
         self.price =price
 
     def __str__(self):
