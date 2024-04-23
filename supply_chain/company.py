055ae1acc99a5949deb58231a5f3679dff992a3b
Francisco Vicente Suarez Bellon <95106875+FVSB@users.noreply.github.com>
2024-04-23 16:10:07 -0400

    Add Company Tags

--- supply_chain/company.py
+++ supply_chain/company.py
@@ -1,5 +1,15 @@
 from abc import ABC, abstractmethod
 from supply_chain.sim_environment import SimEnvironment
+from enum import Enum
+
+
+class TypeCompany(Enum):
+    Matrix = 1
+    BaseProducer = 2
+    SecondaryProvider = 3
+    Logistic = 4
+    Warehouse = 5
+    Store = 6
 
 
 class Company(ABC):
@@ -12,3 +22,10 @@ class Company(ABC):
         self.environment: SimEnvironment = environment
         """The reference to the environment of the simulation"""
         # TODO: Paco, remember that companies offer services. Think in how to model this
+
+
+class LogisticCompany(Company):
+    """
+    Class for the logistic Company
+    """
+    pass
