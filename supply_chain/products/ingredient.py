8e96638891b2ae5bb6044a566866de6bed1250c2
Leismael Sosa <leismael.sosa17@gmail.com>
2024-04-27 14:51:47 -0400

    Modifying the products once more

--- supply_chain/products/ingredient.py
+++ supply_chain/products/ingredient.py
@@ -4,14 +4,16 @@ from product import Product
 class Ingredient:
     """This represents a single ingredient for the creation of a product"""
 
-    def __init__(self, product: Product, amount: int) -> None:
-        self.product: Product = product
-        """The product of the ingredient"""
+    def __init__(self, product_name: str, amount: int) -> None:
+        self.product_name: str = product_name
+        """The name of the product"""
         self.amount: int = amount
         """The amount of units needed"""
 
-    def get_product(self) -> Product:
-        return self.product
+    def get_product_name(self) -> str:
+        """The name of the product that this ingredient needs"""
+        return self.product_name
 
     def get_amount(self) -> int:
+        """The number of units of the product needed for the ingredient"""
         return self.amount
