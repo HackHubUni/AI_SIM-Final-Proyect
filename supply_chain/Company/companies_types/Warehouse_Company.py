acc13759a54051e5fab55ecafb83601beb092303
Francisco Vicente Suarez Bellon <95106875+FVSB@users.noreply.github.com>
2024-04-30 11:12:30 -0400

    reafactor companies

--- supply_chain/Company/companies_types/Warehouse_Company.py
+++ supply_chain/Company/companies_types/Warehouse_Company.py
@@ -1,10 +1,12 @@
+
 from abc import ABC
 from typing import Callable, List
 
 from supply_chain.Company.orders.Sell_order import SellOrder, ProduceOrder
 from supply_chain.Company.orders.delivery_order import DeliveryOrder
-from supply_chain.Company.stock_manager.stock_manager import CompanyStockBase, ProductorCompanyStock, ManufacturingStock, \
-    WarehouseStockManager
+from supply_chain.Company.stock_manager.manofacturing_stock_manager import ManufacturingStock
+from supply_chain.Company.stock_manager.warehouse_stock_manager import WarehouseStockManager
+
 from supply_chain.products.ingredient import Ingredient
 from supply_chain.sim_event import SimEvent
 
@@ -14,40 +16,39 @@ try:
     from supply_chain.products.product import Product
     from supply_chain.Company.registrers.registers import *
     from supply_chain.company import Company, TypeCompany
+    from supply_chain.Company.companies_types.Producer_Company import *
 except:
 
     pass
 
-
-class CompanyWrapped(Company, ABC):
+class WarehouseCompany(CompanyWrapped):
 
     def __init__(self,
                  name: str,
                  get_time: Callable[[], int],
                  add_event: Callable[[SimEvent], None],
-                 stock_manager: CompanyStockBase,
-
-                 ):
-        super().__init__(name, get_time, add_event)
+                 stock_manager: WarehouseStockManager, ):
+        super().__init__(name, get_time, add_event, stock_manager)
         self.register = Registry()
-        self.stock_manager = stock_manager
 
-        self.start()
-        """
-        Call the start function
-        """
-
-    # TODO:Carla aca tienes como saber el tiempo actual
     @property
-    def time(self) -> int:
-        """
-        Retorna el tiempo actual en que se está
-        :return:
-        """
-        return self.get_time()
+    def tag(self):
+        return TypeCompany.Warehouse
 
-    def start(self):
+    def stock(self):
         pass
 
+    def in_storage_product(self, order: Order):
+        """
+        Cuando se entra a guardar un producto
+        :param order:
+        :return:
+        """
+        pass
 
-
+    # TODO: Crear el send recibe un Order
+    def out_storage_product(self, order: Order):
+        """
+        Cuando se saca un producto del almacén
+        :return:
+        """
