2431e5e60021d6f1ad26e939670ba8990ac83f82
Francisco Vicente Suarez Bellon <95106875+FVSB@users.noreply.github.com>
2024-05-01 12:23:47 -0400

    now

--- supply_chain/agents/new/planificar.py
+++ supply_chain/agents/new/planificar.py
@@ -19,8 +19,8 @@ def get_solution(problem):
         lis.append(solution)
     return lis
 def new__():
-    return PlanningProblem(initial='Casa(A)',
-                           goals='Dormir(J,A) ',
+    return PlanningProblem(initial='Pedir(Pizza,Primera)',
+                           goals='Abastecer(Pizza,Primera) ',
                            actions=[  # Action('DD(x,y) ',
                                #       precond='Comprar(x,y)',
                                #       effect='Abastecer(x,y)',
@@ -30,20 +30,71 @@ def new__():
                                #       precond='Casa(y)',
                                #       effect='Dormir(x,y)',
                                #       domain='Casa(y) & Persona(x)'),
+                              # Action('Preguntar_trasnportista_desde_almacen(x,y)',
+                              #        precond='Cant_en_almacen(x,y)',
+                              #        effect='Abastecer(x,y) ',
+                              #        domain=' Producto(x) & Tienda(y)'),
+#
 
-                               Action('C(x,y)',
-                                      precond='Casa(y)',
-                                      effect='Caminarcasa(x,y)',
-                                      domain='Casa(y) & Persona(x)'),
+                               #Action('Preguntar_disponibilidad_almacen(x,y)',
+                               #       precond='Pedir(x,y) & ~Precio_manufacturera(x,y)',
+                               #       effect='Cant_en_almacen(x,y)  ',
+                               #       domain=' Producto(x) & Tienda(y)'),
+#
 
-                               Action('LLEgar(x,y)',
-                                      precond='Casa(y)',
-                                      effect='Dormir(x,y)',
-                                      domain='Casa(y) & Persona(x) '),
+
+
+
+#
+                               Action('Preguntar_distribuidor_desde_almacen(x,y)',
+                                      precond='Preguntar_distribuidor(x,y) & Preguntar_almacen(x,y)',
+                                      effect='Abastecer(x,y)  ',
+                                      domain=' Producto(x) & Tienda(y)'),
+
+                               Action('Preguntar_distribuidormm(x,y)',
+                                      precond='Preguntar_almacen(x,y)',
+                                      effect='Preguntar_distribuidor(x,y)  ',
+                                      domain=' Producto(x) & Tienda(y)'),
+
+
+                               Action('Preguntar_almacen(x,y)',
+                                      precond='Precio_manufacturera(x,y)',
+                                      effect='Preguntar_almacen(x,y) ',
+                                      domain=' Producto(x) & Tienda(y)'),
+
+                               Action('Preguntar_precio_manufacturera(x,y)',
+                                      precond='Pedir(x,y)',
+                                      effect='Precio_manufacturera(x,y) & Cant_en_almacen(x,y)',
+                                      domain='Producto(x) & Tienda(y)'),
+
+#
+#
+#
+#
+#
+#
+#
+                              # Action('Pedir_precio_almacen(x,y)',
+                              #        precond='Precio_manufacturera(x,y)',
+                              #        effect='Precio_almacen(x,y) ',
+                              #        domain='Producto(x)  & Manufacturera(y) & Tienda(z) '),
+#
+                              # Action('Pedir_precio_distribuidor(x,y,z)',
+                              #        precond='Precio_almacen(x,y) & Precio_manufacturera(x,z)',
+                              #        effect='Precio_distribuidor(x,y,z)',
+                              #        domain='Producto(x) & Almacen(y)  & Manufacturera(z)'),
+#
+                              # Action('Pedir_precio_distribuidor(x,y,z)',
+                              #        precond='Pedir(w) & Precio_almacen(x,y) & Precio_manufacturera(x,z) & Precio_distribuidor(x,y,z)',
+                              #        effect='Abastecer(x,w)',
+                              #        domain='Producto(x) & Almacen(y)  & Manufacturera(z) & Tienda(w)'),
+
+
+
 
                            ],
 
-                           domain='Casa(A) & Persona(J)')
+                           domain='Tienda(Primera) & Producto(Pizza) & Manufacturera(Manu) & Distribuidor(Dist) ')
 
 
 
