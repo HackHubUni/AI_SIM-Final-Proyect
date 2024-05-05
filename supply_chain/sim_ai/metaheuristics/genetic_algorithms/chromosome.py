b9c345c724250798c2c6e8410543541e40c1d6a3
Leismael Sosa <leismael.sosa17@gmail.com>
2024-05-05 00:01:25 -0400

    Done with the genetic algorithm

--- supply_chain/sim_ai/metaheuristics/genetic_algorithms/chromosome.py
+++ supply_chain/sim_ai/metaheuristics/genetic_algorithms/chromosome.py
@@ -1,47 +1,67 @@
-from gene import *
+from .gene import *
 
 
-class Chromosome:
-    """Base class for all chromosomes"""
+class Chromosome(ABC):
+    """This is the base class for chromosomes"""
 
     def __init__(
         self,
         genes: list[Gene],
         maximization_problem: bool,
     ) -> None:
-        if genes is None or len(genes) == 0:
-            raise Exception(f"The genes parameter is None or is empty")
         self.genes: list[Gene] = genes
-        """The list of all the genes"""
+        """This are the genes of the chromosome"""
         self.maximization_problem: bool = maximization_problem
-        """Represents if the problem is a maximization problem"""
+        """Says if this chromosome is part of a maximization problem"""
 
     @abstractmethod
     def mate(self, other: Self) -> Self:
-        """This method mates two chromosomes and generate a new chromosome child"""
+        """This method mates a chromosome with another chromosome"""
         pass
 
     @abstractmethod
-    def mutate(self, mutation_rate: float = 0.3):
-        """Mutate this chromosome in place"""
+    def mutate(self) -> Self:
+        """This method returns a copy of this chromosome but with mutated genes"""
         pass
 
     @abstractmethod
-    def calculate_fitness(self) -> float:
-        """A function for calculate the fitness of this chromosome"""
+    def improve(self, n_iterations: int = 10) -> Self:
+        """This method improve a chromosome"""
         pass
 
-    def get_solution(self):
-        """This method converts the genes in a solution of the domain problem"""
+    def get_best(self, other: Self) -> Self:
+        """This method returns the best of the 2 chromosomes"""
+        my_fitness = self.get_fitness()
+        other_fitness = other.get_fitness()
+
+        def cmp(x: float, y: float):
+            return x > y if self.maximization_problem else x < y
+
+        better = self if cmp(my_fitness, other_fitness) else other
+        return better
+
+    def get_solution_values(self) -> list:
+        """This method returns a list with the values of all the genes in the chromosome"""
+        return [gene.value for gene in self.genes]
+
+    @abstractmethod
+    def get_fitness(self) -> float:
+        """This method returns the fitness of this chromosome"""
         pass
 
     @abstractmethod
-    def clone(self, with_mutation: bool) -> Self:
-        """Returns a clone of this chromosome"""
+    def compatible(self, other: Self) -> bool:
+        """This method says if the other Chromosome is compatible for mating"""
         pass
 
-    def equal(self, other_chromosome: Self) -> bool:
-        """Says if two chromosomes are equals, meaning that they could mate"""
-        return len(self.genes) == len(other_chromosome.genes) and all(
-            gene1 == gene2 for gene1, gene2 in zip(self.genes, other_chromosome.genes)
-        )
+    @abstractmethod
+    def random_instance(self) -> Self:
+        """This method returns a random instance of the chromosome"""
+        pass
+
+    @abstractmethod
+    def get_solution(self) -> Any:
+        """This method construct a solution in the terms of the problem.
+        If you want to use this function then you should create a chromosome that implements it
+        """
+        pass
--- supply_chain/sim_ai/metaheuristics/genetic_algorithms/genetic_implementations/basic_genetic_alg.py
+++ supply_chain/sim_ai/metaheuristics/genetic_algorithms/genetic_implementations/basic_genetic_alg.py
@@ -2,17 +2,97 @@ from ..genetic_algorithm import *
 import random as rnd
 
 
-class BasicGeneticAlgorithm(GeneticAlgorithm):
-    """This class implements the genetic algorithm by using single point crossover"""
-
-    def __init__(self, sample_chromosome: Chromosome) -> None:
+class TournamentGeneticAlgorithm(GeneticAlgorithm):
+    def __init__(
+        self,
+        sample_chromosome: Chromosome,
+        tournament_percentage: float,
+    ) -> None:
         super().__init__(sample_chromosome)
+        if not 0 < tournament_percentage <= 1:
+            raise Exception(
+                "The tournament percentage must be between 0 (exclusive) and 1 (inclusive)"
+            )
+        self.tournament_percentage = tournament_percentage
+        """The percentage of the population to take as participants in a tournament"""
+
+    def generate_population(self, population_size: int) -> list[Chromosome]:
+        """This method generates a new population"""
+        population = [
+            self.sample_chromosome.random_instance() for _ in range(population_size)
+        ]
+        return population
+
+    def get_best(
+        self,
+        population_sample: list[Chromosome],
+    ) -> Chromosome:
+        best = population_sample[0]
+        for i in range(1, len(population_sample)):
+            best = best.get_best(population_sample[i])
+        return best
+
+    def select_next_parent_with_tournament(
+        self,
+        population: list[Chromosome],
+        tournament_size: int,
+    ) -> Chromosome:
+        """This method returns a parent by the tournament selection method"""
+        if tournament_size > len(population):
+            raise Exception("The tournament size is greater than the population")
+        tournament_population = rnd.sample(population, tournament_size)
+        best = self.get_best(tournament_population)
+        return best
 
     def selection(
         self, population: list[Chromosome]
     ) -> list[tuple[Chromosome, Chromosome]]:
-        progenitor_list: list[tuple[Chromosome, Chromosome]] = []
+        tournament_size = max(1, int(len(population) * self.tournament_percentage))
+        parents: list[tuple[Chromosome, Chromosome]] = []
         for _ in range(len(population)):
-            idx, idy = rnd.sample(range(len(population)), 2)
-            progenitor_list.append((population[idx], population[idy]))
-        return progenitor_list
+            parent1 = self.select_next_parent_with_tournament(
+                population, tournament_size
+            )
+            parent2 = self.select_next_parent_with_tournament(
+                population, tournament_size
+            )
+            parents.append((parent1, parent2))
+        return parents
+
+    def improve_population(self, population: list[Chromosome]) -> list[Chromosome]:
+        """Improves a population"""
+        improved_chromosomes: list[Chromosome] = []
+        for chromosome in population:
+            improved = chromosome.improve()
+            improved_chromosomes.append(improved)
+        return improved_chromosomes
+
+    def solve(
+        self,
+        population_size: int,
+        max_iterations: int,
+        stop_criteria: Callable[[float], bool],
+    ) -> Chromosome:
+        if population_size <= 5:
+            raise Exception(
+                "The population size must be greater than 5 for the algorithm to work"
+            )
+        if max_iterations <= 5:
+            raise Exception("The maximum number of iterations must be greater than 5")
+        population = self.improve_population(self.generate_population(population_size))
+        best = self.get_best(population)
+        for i in range(max_iterations):
+            if stop_criteria(best.get_fitness()):
+                print(
+                    f"The genetic algorithm stops in the iteration = {i} with the stop criteria"
+                )
+                return best
+            childrens = self.mate_population(population)  # Mating
+            mutated_childrens = [children.mutate() for children in childrens]
+            improved_childrens = [children.improve(5) for children in mutated_childrens]
+            population = improved_childrens
+            best = self.get_best(population)
+        print(
+            f"The algorithm stops after all the {max_iterations} and could not achieve the stop criteria"
+        )
+        return best
